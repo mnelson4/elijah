3b90f8d6833119dbc4e50b664aaab09838e98b4a
Mike Nelson <michael@eventespresso.com>
2016-03-28 22:44:12 -0700

    validate robjective

--- plugins/elijah/includes/templates/years-taxonomy-input.php
+++ plugins/elijah/includes/templates/years-taxonomy-input.php
@@ -3,10 +3,14 @@
 	<input 
 		name="<?php echo elijah_year_input_name( $taxonomy_name, true );?>" 
 		value="<?php echo get_post_meta( $post_id, elijah_year_input_name( $taxonomy_name, true ), true );?>"
-		placeholder="example: 1820"> 
+		placeholder="Year"
+		pattern="\d{4}"
+		title="<?php _e( '4 Digit Year (eg 1969)');?>"> 
 	<label for="<?php echo elijah_year_input_name( $taxonomy_name, false );?>-label"><?php _e( 'and', 'event_espresso');?></label>
 	<input 
 		name="<?php echo elijah_year_input_name( $taxonomy_name, false );?>" 
 		value="<?php echo get_post_meta( $post_id, elijah_year_input_name( $taxonomy_name, false ), true );?>"
-		placeholder="example: 1830">
+		placeholder="Year"
+		pattern="\d{4}"
+		title="<?php _e( '4 Digit Year (eg 1969)');?>">
 </div>
